6a12b78c6382bc59ecda0050e4f157d1b6970fc4
Alper <m.alper.genc@gmail.com>
2020-07-12 13:42:31 +0300

    User object created

--- GroupUp/Models/IdentityModels.cs
+++ GroupUp/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,8 @@ namespace GroupUp.Models
     // ApplicationUser sınıfınıza daha fazla özellik ekleyerek kullanıcıya profil verileri ekleyebilirsiniz. Daha fazla bilgi için lütfen https://go.microsoft.com/fwlink/?LinkID=317594 adresini ziyaret edin.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public User User { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // authenticationType özelliğinin CookieAuthenticationOptions.AuthenticationType içinde tanımlanmış olanla eşleşmesi gerektiğini unutmayın
@@ -20,6 +23,8 @@ namespace GroupUp.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        // Is not named 'Users' because class IdentityDbContext has a DbSet with the given name
+        public DbSet<User> UserInfo { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -28,6 +33,14 @@ namespace GroupUp.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>()
+                .HasRequired(u => u.AspNetIdentity)
+                .WithRequiredPrincipal(a => a.User);
         }
     }
 }
---
+++ GroupUp/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GroupUp.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        public string ContactInfo { get; set; }
+
+        [Required]
+        public int SecurityLevel { get; set; }
+
+        [Required]
+        public int TrustPoints { get; set; }
+
+        [Required]
+        public ApplicationUser AspNetIdentity { get; set; }
+    }
+}
