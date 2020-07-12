feb47b2d1114041307a70312c29566482aef5a4b
Alper <m.alper.genc@gmail.com>
2020-07-12 13:30:24 +0300

    Initial settings
    SignalR installed, default connection for EF set, and initial migrations folder has been created.

---
+++ GroupUp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace GroupUp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GroupUp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(GroupUp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
