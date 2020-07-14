44c064a6828aec021e6f460fc1bca030b1bb3991
Alper <m.alper.genc@gmail.com>
2020-07-14 21:16:38 +0300

    added initial chat function

--- GroupUp/GroupUpHub.cs
+++ GroupUp/GroupUpHub.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using GroupUp.Models;
 using Microsoft.AspNet.SignalR;
 
 namespace GroupUp
 {
     public class GroupUpHub : Hub
     {
+        public readonly ApplicationDbContext _context;
         public GroupUpHub()
         {
-
+            _context = new ApplicationDbContext();
         }
-        public void Hello()
+        public Task JoinRoom(string roomName)
         {
-            Clients.All.hello();
+            return Groups.Add(Context.ConnectionId, roomName);
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.Remove(Context.ConnectionId, roomName);
+        }
+
+        public void Send(string name, string message, string roomName)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.Group(roomName).addNewMessageToPage(name, message);
         }
     }
 }
