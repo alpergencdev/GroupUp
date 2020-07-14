430b6e4cc2ace49d742868fdb475c8b3d2c844a2
Alper <m.alper.genc@gmail.com>
2020-07-14 21:34:02 +0300

    Database now holds chat logs

--- GroupUp/GroupUpHub.cs
+++ GroupUp/GroupUpHub.cs
@@ -28,7 +28,17 @@ namespace GroupUp
         public void Send(string name, string message, string roomName)
         {
             // Call the addNewMessageToPage method to update clients.
-            Clients.Group(roomName).addNewMessageToPage(name, message);
+            try
+            {
+                int roomNo = Int32.Parse(roomName);
+                _context.Groups.Single(g => g.GroupId == roomNo).ChatLog += $"<strong>{name}: </strong>{message}\n";
+                _context.SaveChanges();
+                Clients.Group(roomName).addNewMessageToPage(name, message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
