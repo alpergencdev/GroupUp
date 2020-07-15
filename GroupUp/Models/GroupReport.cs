92f971456bf7720c2c7698870e3da7b4895e5cd7
Alper <m.alper.genc@gmail.com>
2020-07-15 14:31:59 +0300

    Added initial group objects

---
+++ GroupUp/Models/GroupReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GroupUp.Models
+{
+    public class GroupReport
+    {
+        public int GroupReportId { get; set; }
+
+        [Required] public Group TargetGroup { get; set; }
+
+        [Required] public string Reason { get; set; }
+
+        [Required] public string DetailedDescription { get; set; }
+
+
+    }
+}
---
+++ GroupUp/Models/UserReport.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace GroupUp.Models
+{
+    public class UserReport
+    {
+        public int UserReportId { get; set; }
+
+        [Required]
+        public User TargetUser { get; set; }
+
+        [Required]
+        public string Reason { get; set; }
+
+        [Required]
+        public string DetailedDescription { get; set; }
+
+
+    }
+}
