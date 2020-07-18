a9993d907e9795a13a4dfb191e13d7e0cf321992
Alper <m.alper.genc@gmail.com>
2020-07-19 02:13:44 +0300

    added unique e-mail validation to email in UserEditViewModel

--- GroupUp/ViewModels/UserEditViewModel.cs
+++ GroupUp/ViewModels/UserEditViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using GroupUp.Models;
 
 namespace GroupUp.ViewModels
 {
@@ -11,10 +12,28 @@ namespace GroupUp.ViewModels
         [Required]
         [EmailAddress]
         [Display(Name="E-mail Address")]
+        [IsUniqueEmail]
         public string Email { get; set; }
 
         [Required]
         [Display(Name = "Contact Information:")]
         public string ContactInfo { get; set; }
+    }
+
+    public class IsUniqueEmail : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var email = (string) validationContext.ObjectInstance;
+
+            if (context.Users.Any(u => u.AspNetIdentity.Email == email))
+            {
+                return new ValidationResult("The e-mail address you have requested is already in use.");
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
